a080ee441c7147addd8e144fe6df7d74524d25bb
yume arai <dreampotter.0917@icloud.com>
2022-09-30 20:05:19 +0900

    0930

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-$(document).on('ready', function() {
-    $(".full-screen").slick({
-      centerPadding: '10%',
-      autoplay: true,
-      autoplaySpeed: 1000,
-      speed: 1000,
-      infinite: true,
 
+
+  $(function () {
+
+    $(window).scroll(function () {
+      const windowHeight = $(window).height();
+      const scroll = $(window).scrollTop();
+  
+      $('.element').each(function () {
+        const targetPosition = $(this).offset().top;
+        if (scroll > targetPosition - windowHeight + 100) {
+          $(this).addClass("is-fadein");
+        }
+      });
     });
+
+    $('.slider').slick({
+      autoplay: true,         //自動再生
+      autoplaySpeed: 2000,　  //自動再生のスピード
+      speed: 800,     　 //スライドするスピード
+      dots: true,　　　　　　//スライドしたのドット
+      arrows: false,          //左右の矢印
+      infinite: true,　　　　//スライドのループ
+      pauseOnHover: false,   //ホバーしたときにスライドを一時停止しない　
+      centerMode:true,
+	    centerPadding:"20%",
+  });
+    
+
+  
   });
