559694e5592841d45470317ccfd1f3d985c5229b
yume arai <dreampotter.0917@icloud.com>
2022-09-30 09:54:16 +0900

    0929

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).on('ready', function() {
+    $(".full-screen").slick({
+      centerPadding: '10%',
+      autoplay: true,
+      autoplaySpeed: 1000,
+      speed: 1000,
+      infinite: true,
+
+    });
+  });
