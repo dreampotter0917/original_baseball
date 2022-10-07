a793cb9c77d3efab33a97a6f3a4a13b850b8f83b
yume arai <dreampotter.0917@icloud.com>
2022-10-07 13:28:50 +0900

    asease

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-
-
-  $(function () {
-
+$(function(){
+	if (window.matchMedia('(max-width: 668px)').matches) {
+		/* ウィンドウサイズ668以下の処理を記述 */
     $(window).scroll(function () {
       const windowHeight = $(window).height();
       const scroll = $(window).scrollTop();
-  
+
       $('.element').each(function () {
         const targetPosition = $(this).offset().top;
         if (scroll > targetPosition - windowHeight + 100) {
@@ -15,17 +14,39 @@
     });
 
     $('.slider').slick({
-      autoplay: true,         //自動再生
-      autoplaySpeed: 2000,　  //自動再生のスピード
-      speed: 800,     　 //スライドするスピード
-      dots: true,　　　　　　//スライドしたのドット
-      arrows: false,          //左右の矢印
-      infinite: true,　　　　//スライドのループ
-      pauseOnHover: false,   //ホバーしたときにスライドを一時停止しない　
-      centerMode:true,
-	    centerPadding:"20%",
-  });
-    
+      autoplay: true, //自動再生
+      autoplaySpeed: 2000, //自動再生のスピード
+      speed: 800, //スライドするスピード
+      dots: true, //スライドしたのドット
+      arrows: false, //左右の矢印
+      infinite: true, //スライドのループ
+      pauseOnHover: false, //ホバーしたときにスライドを一時停止しない　
+      centerMode: true,
+    });
 
-  
-  });
+	} else if (window.matchMedia('(min-width:669px)').matches) {
+		/* ウィンドウサイズ769以上の処理を記述 */
+    $(window).scroll(function () {
+      const windowHeight = $(window).height();
+      const scroll = $(window).scrollTop();
+
+      $('.element').each(function () {
+        const targetPosition = $(this).offset().top;
+        if (scroll > targetPosition - windowHeight + 100) {
+          $(this).addClass("is-fadein");
+        }
+      });
+    });
+
+    $('.slider').slick({
+      autoplay: true, //自動再生
+      autoplaySpeed: 2000, //自動再生のスピード
+      speed: 800, //スライドするスピード
+      dots: true, //スライドしたのドット
+      arrows: false, //左右の矢印
+      infinite: true, //スライドのループ
+      pauseOnHover: false, //ホバーしたときにスライドを一時停止しない　
+      centerMode: true,
+      centerPadding: "20%",
+    });
+}})
